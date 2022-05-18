47b542237f7cde5181b03bf352bf459e31cb8740
Adem VAROL <ademvarol@posta.mu.edu.tr>
2022-05-18 22:43:35 +0300

    PL Inheritance Homework

--- src/ShapeDemo.java
+++ src/ShapeDemo.java
@@ -4,16 +4,15 @@ public class ShapeDemo {
         // Shape circle = new Circle(3,"blue",false);
         // Shape circle = new Circle(4);
         Shape circle = new Circle();
-        System.out.println(circle.toString());
+        System.out.println(circle);
         System.out.println("Area of circle         = " + Math.round(circle.getArea() * 100.0) / 100.0);
         System.out.println("Perimeter of circle    = " + Math.round(circle.getPerimeter() * 100.0) / 100.0);
         System.out.println();
 
-
         //Shape rect = new Rectangle(3,4,"blue",false);
         //Shape rect = new Rectangle(5,6);
         Shape rect = new Rectangle();
-        System.out.println(rect.toString());
+        System.out.println(rect);
         System.out.println("Area of rectangle      = " + rect.getArea());
         System.out.println("Perimeter of rectangle = " + rect.getPerimeter());
         System.out.println();
@@ -21,7 +20,7 @@ public class ShapeDemo {
         //Shape sq = new Square(7,"blue",true);
         //Shape sq = new Square(8);
         Shape sq = new Square();
-        System.out.println(sq.toString());
+        System.out.println(sq);
         System.out.println("Area of square         = " + sq.getArea());
         System.out.println("Perimeter of square    = " + sq.getPerimeter());
 
